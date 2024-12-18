ae11a53354af8f0d584c63163cdcf10136861662
RaulCoretB <raulcoret8@gmail.com>
2024-12-18 18:08:00 +0100

    nuevo

--- Test2024-25/Assets/Test2D_Root/Scripts/PlayerController2D.cs
+++ Test2024-25/Assets/Test2D_Root/Scripts/PlayerController2D.cs
@@ -1,33 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.InputSystem; //Librería necesaria para leer el New Input System
 
 public class PlayerController2D : MonoBehaviour
 {
-    //Referencias privadas generales.
+    //Referencias privadas generales
     [SerializeField] Rigidbody2D playerRb;
     [SerializeField] PlayerInput playerInput;
-    Vector2 moveInput; //Variables para referencar el input de los controladores
+    Vector2 moveInput; //Variable para referenciar el input de los controladores
 
     [Header("Movement Parameters")]
     public float speed;
     [SerializeField] bool isFacingRight;
 
-    [Header("Jump parameters")]
+    [Header("Jump Parameters")]
     public float jumpForce;
-    [SerializeField] private bool isGrounded;
+    [SerializeField] bool isGrounded;
     [SerializeField] GameObject groundCheck;
     [SerializeField] float groundCheckRadius;
     [SerializeField] LayerMask groundLayer;
 
 
+
     // Start is called before the first frame update
     void Start()
     {
-      //Praa autpreferirse: nombre de variabl e= GetComponent<tipo de variable>()
-      playerRb = GetComponent<Rigidbody2D>();
-      playerInput = GetComponent<PlayerInput>();
+        //Para autoreferenciar: nombre de variable = GetComponent<tipo de variable>()
+        playerRb = GetComponent<Rigidbody2D>();
+        playerInput = GetComponent<PlayerInput>();
         isFacingRight = true;
     }
 
@@ -35,19 +36,21 @@ public class PlayerController2D : MonoBehaviour
     void Update()
     {
         GroundCheck();
-        if (moveInput.x > 0 && !isFacingRight) flip();
-        if (moveInput.x < 0 && isFacingRight) flip();
+        if (moveInput.x > 0 && !isFacingRight) Flip();
+        if (moveInput.x < 0 && isFacingRight) Flip();
     }
 
     private void FixedUpdate()
     {
         Movement();
     }
+
     void Movement()
     {
         playerRb.velocity = new Vector3(moveInput.x * speed, playerRb.velocity.y, 0);
     }
-    void flip()
+
+    void Flip()
     {
         Vector3 currentScale = transform.localScale;
         currentScale.x *= -1;
@@ -55,35 +58,31 @@ public class PlayerController2D : MonoBehaviour
         isFacingRight = !isFacingRight;
     }
 
-
     void GroundCheck()
     {
-        //isGrounded es verdadero cuando el circulo detector toque la layer ground.
+        //isGrounded es verdadero cuando el círculo detector toque la layer ground
         isGrounded = Physics2D.OverlapCircle(groundCheck.transform.position, groundCheckRadius, groundLayer);
     }
 
-
-
     #region Input Methods
-    // metodos que permiten leer el inpt de New Input System
-    //crearemos un metodo por cada accion
+    //Métodos que permiten leer el input del New Input System
+    //Crearemos un método por cada acción
 
-    public void HandLeMovemnet(InputAction.CallbackContext context)
-    { 
-    //las acciones de tipo VALUE deben almacenarse = RealValue
-    moveInput = context.ReadValue<Vector2>();
-    }
-    
-
-    public void HandLeJump(InputAction.CallbackContext context)
+    public void HandleMovement(InputAction.CallbackContext context)
     {
-        if(context.started)
+        //Las acciones de tipo VALUE deben almacenarse = ReadValue
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    public void HandleJump(InputAction.CallbackContext context)
+    {
+        if (context.started)
         {
             if (isGrounded)
             {
                 playerRb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
             }
-          
+            
         }
 
     }
@@ -91,8 +90,6 @@ public class PlayerController2D : MonoBehaviour
 
 
     #endregion
-
-
 
 
 
